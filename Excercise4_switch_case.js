2b82e669c69c456de9bebfaf07cbb5d1935655f8
ridozaen <ridozaen@gmail.com>
2018-03-29 18:11:10 +0700

    modify code to console.log outside the switch-case

--- Excercise4_switch_case.js
+++ Excercise4_switch_case.js
@@ -9,45 +9,48 @@ expected result : 1 Januari 1945
 var tanggal = 1;
 var bulan = 1;
 var tahun = 1945;
+var namaBulan = '';
 
 switch (bulan) {
     case 1:
-        console.log(tanggal + ' Januari ' + tahun);
+        namaBulan = 'Januari';
         break;
     case 2:
-        console.log(tanggal + ' Februari ' + tahun);
+        namaBulan = 'Februari';
         break;
     case 3:
-        console.log(tanggal + ' Maret ' + tahun);
+        namaBulan = 'Maret';
         break;
     case 4:
-        console.log(tanggal + ' April ' + tahun);
+        namaBulan = 'April';
         break;
     case 5:
-        console.log(tanggal + ' Mei ' + tahun);
+        namaBulan = 'Mei';
         break;
     case 6:
-        console.log(tanggal + ' Juni ' + tahun);
+        namaBulan = 'Juni';
         break;
     case 7:
-        console.log(tanggal + ' Juli ' + tahun);
+        namaBulan = 'Juli';
         break;
     case 8:
-        console.log(tanggal + ' Agustus ' + tahun);
+        namaBulan = 'Agustus';
         break;
     case 9:
-        console.log(tanggal + ' September ' + tahun);
+        namaBulan = 'September';
         break;
     case 10:
-        console.log(tanggal + ' Oktober ' + tahun);
+        namaBulan = 'Oktober';
         break;
     case 11:
-        console.log(tanggal + ' November ' + tahun);
+        namaBulan = 'November';
         break;
     case 12:
-        console.log(tanggal + ' Desember ' + tahun);
+        namaBulan = 'Desember';
         break;
     default:
         console.log('Masukan format tanggal yg benar!');
         break;
 }
+
+console.log (tanggal + ' ' + namaBulan + ' ' + tahun);
