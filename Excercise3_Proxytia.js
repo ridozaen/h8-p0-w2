1be4b61ef30d7cbbca7f028e97f9ad3ea5ff9f5f
ridozaen <ridozaen@gmail.com>
2018-03-29 16:31:13 +0700

    modify code exercise 3

--- Excercise3_Proxytia.js
+++ Excercise3_Proxytia.js
@@ -5,22 +5,21 @@ var nama = 'Malik';
 var peran = 'Tabib';
 */
 
-var nama = 'Malik';
-var peran = 'Tabib';
+var nama = 'Simbah';
+var peran = 'Penyihir';
 
 if (!nama) {
     console.log("Nama Harus Diisi!");
 }
 else if (!peran) {
     console.log('Halo ' + nama + ', ' + "Pilih peranmu untuk memulai game!");
-} else if (peran == "Ksatria") {
+} else if (peran === "Ksatria") {
     console.log("Selamat datang di dunia Proxytia, " + nama);
     console.log("Halo Ksatria " + nama + ", kamu dapat menyerang dengan senjatamu!");
-} else if (peran == "Tabib") {
+} else if (peran === "Tabib") {
     console.log("Selamat datang di dunia Proxytia, " + nama);
     console.log("Halo Tabib " + nama + ", kamu dapat membantu temanmu yang terluka.");
-} else if (peran == "Penyihir") {
+} else if (peran === "Penyihir") {
     console.log("Selamat datang di dunia Proxytia, " + nama);
     console.log("Halo Penyihir " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
 }
-
