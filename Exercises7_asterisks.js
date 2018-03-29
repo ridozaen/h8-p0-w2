35aff766ed61c922c0696c625f8f1c7d2e283ad3
ridozaen <ridozaen@gmail.com>
2018-03-29 20:28:30 +0700

    modify file excercise 7 asteriks looping add task 3

--- Exercises7_asterisks.js
+++ Exercises7_asterisks.js
@@ -30,3 +30,20 @@ while (counter2 < row2) {
   counter2 = counter2 + 1;
 }
 
+/*
+3. Menyusun Barisan Tangga Bintang
+*/
+var row3 = 10; // input the numbers of rows
+var counter4 = 0;
+var counter5 = 0;
+var asteriks1 = '';
+
+// do loops to display asterisks in the console
+while (counter4 < row3) {
+  counter4 = counter4 + 1;
+  while (counter5 < counter4){
+    asteriks1 = asteriks1 + '*'
+    counter5 = counter5 + 1
+  }
+  console.log(asteriks1);
+}
